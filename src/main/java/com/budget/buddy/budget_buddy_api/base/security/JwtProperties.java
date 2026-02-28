8b2e52fea33a5eebcc6c8eed72ec385eb5ecb1c8
Hleb Ramniou <g.remniov@gmail.com>
2026-02-28 21:13:35 +0200

    Split config between profiles

--- src/main/java/com/budget/buddy/budget_buddy_api/base/security/JwtProperties.java
+++ src/main/java/com/budget/buddy/budget_buddy_api/base/security/JwtProperties.java
@@ -2,7 +2,7 @@ package com.budget.buddy.budget_buddy_api.base.security;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("jwt")
+@ConfigurationProperties(prefix = "security.jwt")
 public record JwtProperties(String secret, long accessTokenValiditySeconds, long refreshTokenValiditySeconds) {
 
 }
